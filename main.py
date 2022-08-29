23ba0b77fa669965911e156259468dfc12dc1ad7
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-08-29 14:09:22 -0500

    sort and flatten logic works

---
+++ main.py
@@ -0,0 +1,34 @@
+#Initialize array     
+my_array = [[1,2], [6,5,4], [9,12,10,11]]
+
+flatten_list = []
+
+# flatten array
+for nums in my_array:
+    for num in nums:
+        flatten_list.append(num)
+print(flatten_list)
+
+arr = flatten_list;     
+base = 0;    
+     
+#Displaying elements of original array    
+print("Elements of original array: ");    
+for i in range(0, len(arr)):    
+    print(arr[i], end=" ");    
+     
+#Sort the array in ascending order    
+for i in range(0, len(arr)):    
+    for j in range(i+1, len(arr)):    
+        if(arr[i] > arr[j]):    
+            base = arr[i];    
+            arr[i] = arr[j];    
+            arr[j] = base;    
+     
+print();    
+     
+#Displaying elements of the array after sorting    
+    
+print("Elements of array sorted in ascending order: ");    
+for i in range(0, len(arr)):    
+    print(arr[i], end=" "); 
